25898baaf2b1c796bb093a35891784e21a5b6b45
Elle <ellesmcfarlane@gmail.com>
2023-11-29 13:34:10 +0100

    update job name..

--- jobscript.sh
+++ jobscript.sh
@@ -3,7 +3,7 @@
 ### –- specify queue --
 #BSUB -q gpuv100
 ### -- set the job Name --
-#BSUB -J tru_fov0_epi_12
+#BSUB -J fov0_epi_fix
 ### -- ask for number of cores (default: 1) --
 #BSUB -n 4
 ### -- specify that the cores must be on the same host -- 
